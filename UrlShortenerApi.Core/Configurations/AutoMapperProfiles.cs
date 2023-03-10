c278733f85241e2a5c97b16776d1109d124efe0b
JLW157 <alexbiletskyi17@gmail.com>
2023-03-10 09:43:27 +0200

    bug with dependencies fixed

--- UrlShortenerApi.Core/Configurations/AutoMapperProfiles.cs
+++ UrlShortenerApi.Core/Configurations/AutoMapperProfiles.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UrlShortenerApi.Models;
 using UrlShortenerApi.Models.DTO_s;
 
 namespace UrlShortenerApi.Core.Configurations
